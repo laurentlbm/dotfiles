661c0952a4daebac222e04985831edda914b8610
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2022-04-11 11:33:43 -0400

    remove trailing space

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -23,7 +23,7 @@ lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"                           
+lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
