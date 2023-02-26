7e161e55befc2bf941bc4ddc33679d446e4aeef3
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2023-02-26 13:41:02 -0500

    add missing files

---
+++ .config/zsh/aliases/init.zsh
@@ -0,0 +1,27 @@
+(( $+commands[paru] )) && autoload -Uz pacf
+(( $+commands[pkg] )) && autoload -Uz pkgf
+(( $+commands[rg] )) && autoload -Uz rgf
+
+(( $+commands[exa] )) && {
+  alias ls='exa --all --git --icons --group-directories-first --time-style=long-iso --color-scale --classify'
+  alias ll='ls --long'
+  alias tree='ls --tree --ignore-glob=.git'
+}
+
+# Define aliases.
+alias sudo='sudo ' # https://wiki.archlinux.org/title/Sudo#Passing_aliases
+alias m='micro'
+(( $+commands[nvim] )) && alias vi='nvim'
+(( $+commands[lvim] )) && alias vi='lvim'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+alias x='exit'
+alias yt='yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"'
+alias g='git'
+alias lzg='lazygit'
+# disable ssh compression for improved rsync speeds
+alias rsync='rsync -haP --numeric-ids -e "ssh -o Compression=no"'
+alias sc='sudo systemctl'
+alias ypull='yadm pull; yadm submodule update --init --recursive --force --remote'
+(( $+commands[codium] )) && alias code=codium
