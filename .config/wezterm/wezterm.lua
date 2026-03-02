b1b2eb4e49804500c0fe09b182710e26fb6cd002
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2026-03-02 11:38:06 -0500

    fix(wezterm): Update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,37 +1,56 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
-local c = {}
-if wezterm.config_builder then
-  c = wezterm.config_builder()
-  c:set_strict_mode(true)
-end
-
-require("fonts").apply(c)
--- require("keys").apply(c)
-require("theme").apply(c)
+config.default_prog = { '/usr/sbin/zsh' }
 
 -- disable tmux
---c.set_environment_variables = {
+--config.set_environment_variables = {
 --  NO_TMUX = "1",
 --}
--- etc.
-c.adjust_window_size_when_changing_font_size = false
-c.audible_bell = "Disabled"
-c.clean_exit_codes = { 130 }
-c.default_cursor_style = "BlinkingBar"
-c.launch_menu = {
-  { args = {"btop"} },
-  { label = "Bash", args = {"bash", "-l"} },
-  { args = {"ssh", "spica"} },
-  { args = {"ssh", "vega"} },
-  { args = {"ssh", "delphi"} },
-}
-c.command_palette_font_size = 14.0
-c.window_background_opacity = 0.85
-c.window_frame = {
-  font_size = 11.0,
+config.adjust_window_size_when_changing_font_size = false
+config.audible_bell = "Disabled"
+config.clean_exit_codes = { 130 }
+
+config.font_size = 11
+config.font = wezterm.font_with_fallback { 'Maple Mono NF' }
+config.color_scheme = 'Catppuccin Mocha'
+config.default_cursor_style = "BlinkingBar"
+-- config.command_palette_font_size = 14.0
+config.window_background_opacity = 0.85
+
+config.launch_menu = {
+  { label = 'Bash', args = { 'bash', '-l' }, }
 }
 
--- require("bar").apply_to_config(c)
+-- -- Title/tab bar
+config.window_frame = {
+  active_titlebar_bg = '#181825',
+  inactive_titlebar_bg = '#181825',
+}
+config.colors = {
+  tab_bar = {
+    background = '181825',
+    active_tab = {
+      bg_color = '#f9c096',
+      fg_color = '#15121c',
+    },
+    inactive_tab = {
+      bg_color = '#332e41',
+      fg_color = '#a4aec8',
+    },
+    inactive_tab_hover = {
+      bg_color = '#332e41',
+      fg_color = '#dadae8',
+    },
+    new_tab = {
+      bg_color = '#332e41',
+      fg_color = '#a4aec8',
+    },
+    new_tab_hover = {
+      bg_color = '#332e41',
+      fg_color = '#dadae8',
+    },
+  }
+}
 
-return c
+return config
